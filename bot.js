76a730c4ec3f5cadadaf9bc96e9fb1a8afbe38e1
hkktjmggfvhgbggv <50679278+hkktjmggfvhgbggv@users.noreply.github.com>
2019-05-17 22:16:48 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Smile By : L o R d BAy Discord Login.in");
+
+
+
+client.on('message', message => {
+     if (message.content === ".") {
+client.user.setGame(`3032#`,'https://www.twitch.tv/Zj_j8')
+ 
+}
+});
+   
+client.login("NDY0NzUyMjMyODE1MTMyNzAz.XN5ZnA.1ZfGwMkb7CWTbe1ZDOlfZueTokk");
