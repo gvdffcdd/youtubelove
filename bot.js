e4336eb4c269afa2e741fc0cbb28cbcce1504b36
gvdffcdd <50933252+gvdffcdd@users.noreply.github.com>
2019-05-27 05:21:00 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "#";
+client.on('ready', () => {
+	    client.user.setStatus('dnd');
+		R7.user.setGame(`Happy .... Day`,'https://www.twitch.tv/hix')
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : faisal`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : faisal ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+
+
+  
+});
+
+
+client.login("NDY0NzUyMjMyODE1MTMyNzAz.XOn2sQ.IIxtG0DpAOhVfHlGXk5B-L_q7Bg");﻿
